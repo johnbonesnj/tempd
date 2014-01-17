c1815fb8b1de86cdccccffe09ad3d2713af17a3b
johnbonesnj <johnbonesnj@gmail.com>
2014-01-16 23:39:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tempd::Application.config.secret_key_base = '8aabf064c0fea0a42b69c558bb5038168bf1156123965c5e5668e13c9c91946541193d531824277106e21cd75bbafa957535f2b947b94cef436405e635263042'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tempd::Application.config.session_store :cookie_store, key: '_tempd_session'
